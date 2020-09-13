500d5fe47d7b0eb72a7f5647fe9fcc18f3aa7721
Mihael Medan <mmedan@xtreme.bet>
2020-09-13 19:30:28 +0200

    removed multithreading tests

--- simple_game.cpp
+++ simple_game.cpp
@@ -6,13 +6,6 @@
 
 #include "src/state_handler/state_manager.h"
 
-using namespace std::chrono_literals;
-
-void system_call_from_thread()
-{
-    std::cout << "Thread function\n";
-}
-
 int main(void)
 {
     InitWindow(800, 600, "raylib template - simple game");
